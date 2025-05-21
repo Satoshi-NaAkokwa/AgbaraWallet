c4c6e60d6a1fc99ffc8b4ff2c02d8b6ae63c65ec
Edu <ebardaji@gmail.com>
2025-05-21 10:25:44 +0200

    Add principal docs (#432)
    * Add principal docs
    
    * Add BRC-20 description

--- types/fungibleTokens.ts
+++ types/fungibleTokens.ts
@@ -10,6 +10,20 @@ export type FungibleToken = BaseToken & {
   balance: string;
   total_sent: string;
   total_received: string;
+
+  /**
+   * Originally used to identify the Stacks contract issuing a token, and is now
+   * used as a context-specific general-purpose ID of the token's definition:
+   *
+   * - Stacks: the [contract
+   *   principal](https://github.com/stacksgov/sips/blob/main/sips/sip-005/sip-005-blocks-and-transactions.md)
+   *   issuing the token.
+   * - Starknet: the
+   *   [CAIP-10](https://github.com/ChainAgnostic/CAIPs/blob/main/CAIPs/caip-10.md)
+   *   designation of the contract issuing the token.
+   * - Runes: the rune ID. E.g., DOG•GO•TO•THE•MOON is 840000:3.
+   * - BRC-20: The ticker, as defined by the protocol's `tick` property.
+   */
   principal: string;
   assetName: string;
   decimals?: number;
